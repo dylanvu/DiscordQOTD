0b9e00754156ce4f21c697fef7b5ab8f041747e8
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-05-21 15:27:49 -0700

    Working Profanity Filter

--- redditqotd.js
+++ redditqotd.js
@@ -1,12 +1,14 @@
 import Axios from 'axios'
 import Discord from "discord.js"
 import cron from 'cron'
+import Filter from 'bad-words'
 
 import dotenv from 'dotenv'
 
 const client = new Discord.Client();
+const filter = new Filter();
+filter.addWords('Reddit', 'reddit', 'redditor', 'karma', 'subreddit', 'repost'); // Since we're stealing questions from r/AskReddit, filter out some reddit-related words
 dotenv.config();
-
 const BOT_TOKEN = process.env.TOKEN;
 
 const GetMessageIDs = (msg) => {
@@ -17,9 +19,35 @@ const GetMessageIDs = (msg) => {
 
 
 async function GetAndSendQuestion() {
-    const TOP_POST_API = "https://www.reddit.com/r/askreddit/top.json"
+    const TOP_POST_API = "https://www.reddit.com/r/askreddit/top.json";
+    // TODO: Error catching
     let post = await Axios.get(TOP_POST_API);
-    let question = "**QOTD: " + post.data.data.children[0].data.title + "**";
+    let question;
+    let profanity = true;
+    let i = 0;
+    // See if the top 25 posts are profane free, and get the first one
+    while (profanity || i > 25) {
+        question = "**QOTD: " + post.data.data.children[i].data.title + "**";
+        profanity = filter.isProfane(question);
+        //console.log(profanity);
+        i++;
+    }
+    // If, for some reason, the top 25 results ALL have profanity, try the top 100 posts
+    if (profanity) {
+        while (profanity || i > 100) {
+            post = await Axios.get(TOP_POST_API + "?limit=100");
+            question = "**QOTD: " + post.data.data.children[i].data.title + "**";
+            profanity = filter.isProfane(question);
+            i++;
+        }
+    }
+    // If the top 100 posts ALL have profanity, throw a general error
+    if (profanity) {
+        question = "404 Question not found. Please make your own QOTD this time, sorry!";
+    }
+
+    // TODO: Randomize between non-profane questions? So people who commonly use Reddit will see a relatively new question?
+
     for (let [currentChannelid, currentGuildid] of channelIdlist) {
         client.channels.cache.get(currentChannelid).send(question);
     }
@@ -53,6 +81,21 @@ client.on("message", msg => {
     if (msg.content === "!qotd_test"){
         // Remove later since this command sends to every channel on the list
         GetAndSendQuestion();
+    }
+
+    // Joke Test
+    if (msg.content === "!qotd_testing"){
+        // Remove later since this command sends to every channel on the list
+        for (let [currentChannelid, currentGuildid] of channelIdlist) {
+            client.channels.cache.get(currentChannelid).send("Critical Error. Please code me better next time.");
+        }
+        setTimeout(() => {
+            for (let [currentChannelid, currentGuildid] of channelIdlist) {
+                client.channels.cache.get(currentChannelid).send("I'm just kidding. Here's your question of the day");
+            }
+            GetAndSendQuestion();
+        }, 10000);
+        
     }
     
     // Remove scheduled QOTD from current channel
