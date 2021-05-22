d179fe7b48dfa4880d8dbbb6cb861975b979de17
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-05-21 23:45:21 -0700

    Working adding and removal from MongoDB

--- redditqotd.js
+++ redditqotd.js
@@ -1,22 +1,27 @@
+import dotenv from 'dotenv'
 import Axios from 'axios'
 import Discord from "discord.js"
 import cron from 'cron'
 import Filter from 'bad-words'
+import mongo from 'mongodb'
 
-import dotenv from 'dotenv'
-import express from 'express';
+import express from 'express'
 
 const APP = express();
 const PORT = 3000;
 
+dotenv.config();
+
 APP.get('/', (req, res) => res.send('Hello World!'));
-APP.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
+APP.listen(PORT, () => console.log(`Discord QOTD app listening at http://localhost:${PORT}`));
 
 const client = new Discord.Client();
+const BOT_TOKEN = process.env.TOKEN;
+
 const filter = new Filter();
 filter.addWords('Reddit', 'reddit', 'redditor', 'karma', 'subreddit', 'repost'); // Since we're stealing questions from r/AskReddit, filter out some reddit-related words
-dotenv.config();
-const BOT_TOKEN = process.env.TOKEN;
+
+const mongoclient = new mongo.MongoClient(process.env.MONGO_DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const GetMessageIDs = (msg) => {
     let channelid = msg.channel.id;
@@ -24,8 +29,27 @@ const GetMessageIDs = (msg) => {
     return [channelid, guildid]
 }
 
+// Schedule a message using cron
+// 0 0 9 * * * means 9:00 AM exactly
+let testJob = new cron.CronJob('0 0 9 * * *', () => {
+    console.log("Sending QOTD");
+    GetAndSendQuestion();
+}, null, true, 'America/Los_Angeles');
+
+testJob.start();
 
 async function GetAndSendQuestion() {
+
+    try {
+        // Connect to MongoDB Cluster
+        await mongoclient.connect();
+        await AddChannelIfExists(mongoclient);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        //await mongoclient.close();
+    }
+
     const TOP_POST_API = "https://www.reddit.com/r/askreddit/top.json";
     // TODO: Error catching
     let post = await Axios.get(TOP_POST_API);
@@ -60,61 +84,104 @@ async function GetAndSendQuestion() {
     }
 }
 
+async function AddChannelToDatabase(mongoclient, channelid, guildid, msg) {
+    try {
+        // Connect to MongoDB Cluster
+        await mongoclient.connect();
+        await AddChannelIfExists(mongoclient, channelid, guildid, msg);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        //await mongoclient.close();
+    }
+}
+
+async function AddChannelIfExists(mongoclient, channelid, guildid, msg) {
+
+    let channelCollection = await mongoclient.db().collection("ActiveChannels");
+
+    // Check if the channel exists in the database
+    let someCursor = await channelCollection.findOne(
+        {
+            channel_id : channelid,
+            guild_id : guildid
+        }
+    )
+    if (!someCursor) {
+        console.log("Adding new channel");
+        msg.reply("Starting QOTD!");
+        channelCollection.insertOne({
+            channel_id : channelid,
+            guild_id : guildid
+        })
+    } else {
+        console.log("Channel already exists");
+        msg.reply("QOTD has already been started. Please wait until 9:00 AM PST!");
+    }
+}
+
+async function RemoveChannelFromDatabase(channelid, guildid, msg) {
+    try {
+        // Connect to MongoDB Cluster
+        await mongoclient.connect();
+        await RemoveChannelIfExists(mongoclient, channelid, guildid, msg);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        //await mongoclient.close();
+    }
+}
+
+async function RemoveChannelIfExists(mongoclient, channelid, guildid, msg) {
+
+    let channelCollection = await mongoclient.db().collection("ActiveChannels");
+
+    // Check if the channel exists in the database
+    let someCursor = await channelCollection.findOne(
+        {
+            channel_id : channelid,
+            guild_id : guildid
+        }
+    )
+    if (someCursor) {
+        console.log("Deleting channel");
+        msg.reply("Stopping QOTD in this channel... D:");
+        channelCollection.deleteOne({
+            channel_id : channelid,
+            guild_id : guildid
+        }, true)
+    } else {
+        console.log("Cannot delete channel that doesn't exist");
+        msg.reply("It appears you haven't added QOTD to this channel yet. At least try me out before removing me... D:");
+    }
+}
+
 // idList will hold every channel-guild id key-value pair
 let channelIdlist = new Map();
-
-// Schedule a message
-// 0 0 9 * * * means 9:00 AM exactly
-let testJob = new cron.CronJob('0 0 9 * * *', () => {
-    console.log("Sending QOTD");
-    GetAndSendQuestion();
-}, null, true, 'America/Los_Angeles');
-
-testJob.start();
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-// Control the sending of messages
+
+// <-------------------------------------- Control the sending of messages --------------------------------------------->
 client.on("message", msg => {
     // Starting the Bot
     if (msg.content === "!qotd_start") {
+        // Add the channel to the mongodb database
         let [channelid, guildid] = GetMessageIDs(msg);
-        if (!channelIdlist.has(channelid)) {
-            msg.reply("Starting QOTD!");
-            channelIdlist.set(channelid, guildid);
-        } else {
-            msg.reply("QOTD has already been started. Please wait until 9:00 AM PST!");
-        }
-        
+        AddChannelToDatabase(mongoclient, channelid, guildid, msg);
+    }
+    
+    // Remove scheduled QOTD from current channel
+    if (msg.content ==="!qotd_stop") {
+        let [channelid, guildid] = GetMessageIDs(msg);
+        RemoveChannelFromDatabase(channelid, guildid, msg);
     }
 
     if (msg.content === "!qotd_test"){
         // Remove later since this command sends to every channel on the list
         GetAndSendQuestion();
-    }
-
-    // Joke Test
-    if (msg.content === "!qotd_testing"){
-        // Remove later since this command sends to every channel on the list
-        for (let [currentChannelid, currentGuildid] of channelIdlist) {
-            client.channels.cache.get(currentChannelid).send("Critical Error. Please code me better next time.");
-        }
-        setTimeout(() => {
-            for (let [currentChannelid, currentGuildid] of channelIdlist) {
-                client.channels.cache.get(currentChannelid).send("I'm just kidding. Here's your question of the day");
-            }
-            GetAndSendQuestion();
-        }, 10000);
-        
-    }
-    
-    // Remove scheduled QOTD from current channel
-    if (msg.content ==="!qotd_stop") {
-        msg.reply("Stopping QOTD in this channel... D:");
-        // Remove the channel id from the array of ids
-        channelIdlist.delete(msg.channel.id);
     }
 
     // Instantly Generating another question
@@ -129,6 +196,22 @@ client.on("message", msg => {
     if (msg.content === "!qotd_help") {
         msg.reply("To start daily QOTD, do !qotd_start \n To stop daily QOTD, do !qotd_stop \n To view the code, do !qotd_github ");
     }
+
+    
+    // Joke Test
+    if (msg.content === "!qotd_testing"){
+        // Remove later since this command sends to every channel on the list
+        for (let [currentChannelid, currentGuildid] of channelIdlist) {
+            client.channels.cache.get(currentChannelid).send("npm ERR! code ELIFECYCLE \n npm ERR! errno 1 \n npm ERR! discordqotd@1.0.0 start: `node redditqotd.js` \n npm ERR! \n npm ERR! Failed at the discordqotd@1.0.0 start script. \n npm ERR! This is probably not a problem with npm. There is likely additional logging output above.");
+        }
+        setTimeout(() => {
+            for (let [currentChannelid, currentGuildid] of channelIdlist) {
+                client.channels.cache.get(currentChannelid).send("hahajkheresyourquestionoftheday");
+            }
+            GetAndSendQuestion();
+        }, 10000);
+        
+    }
 })
 
 client.login(BOT_TOKEN);
@@ -138,3 +221,6 @@ client.login(BOT_TOKEN);
 // TODO: Randomly select from top posts?
 // TODO: Integrate custom questions?
 // TODO: Save questions to a database, incorporate a database mode?
+// TODO: Fix the warnings, like [MONGODB DRIVER] Warning: the options [servers] is not supported
+// TODO: Handle deleted channels?
+// TODO: Make functions nicer somehow
