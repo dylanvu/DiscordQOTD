9524cfba491c178e4d0d213ef1ce81d50770b4fb
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-07-09 22:54:44 -0700

    Implemented CS Question of the Day

--- redditqotd.js
+++ redditqotd.js
@@ -6,6 +6,7 @@ import Filter from 'bad-words'
 import mongo from 'mongodb'
 import express from 'express'
 import JSON_FILTER from "./filteredwords.json"
+import fetch from 'node-fetch'
 // Note: start with a `--experimental-json-modules`
 
 import GetNonProfaneQuestion from './GetNonProfaneQuestion.js'
@@ -48,11 +49,27 @@ const GetMessageIDs = (msg) => {
 let testJob = new cron.CronJob('0 0 9 * * *', () => {
     console.log("Sending question to all channels");
     GetAndSendQuestion();
+    GetAndSendCSQuestion();
 }, null, true, 'America/Los_Angeles');
 
 testJob.start();
 
-// <--------------------------------------- Question Sending Functions --------------------------------------->
+// <--------------------------------------- QOTD Question Sending Functions --------------------------------------->
+
+async function IterateAndSendToAll(mongoclient, collectionName, question, questionTosave) {
+    let channelCollection = await mongoclient.db().collection(collectionName);
+    let allCursor = await channelCollection.find();
+
+    // Reset previous question for every channel to be the current top question
+    channelCollection.update({}, { $set: {
+        prev_question : [questionTosave]
+    }});
+
+    allCursor.forEach((thisChannel) => {
+        // TODO: Add channel deletion check here and removal from collection
+        client.channels.cache.get(thisChannel.channel_id).send(question);
+    })
+}
 
 async function GetAndSendQuestion() {
 
@@ -93,19 +110,21 @@ async function SendQuestionToAllChannels(mongoclient) {
     }
 
     // TODO: Randomize between non-profane questions? So people who commonly use Reddit will see a relatively new question?
-    // Now, use MongoDB to get active channels
-    let channelCollection = await mongoclient.db().collection("ActiveChannels");
-    let allCursor = await channelCollection.find();
 
-    // Reset previous question for every channel to be the current top question
-    channelCollection.update({}, { $set: {
-        prev_question : [questionTosave]
-    }});
+    // Now, use MongoDB to get active channels and send
+    IterateAndSendToAll(mongoclient, "ActiveChannels", question, questionTosave);
+    // let channelCollection = await mongoclient.db().collection("ActiveChannels");
+    // let allCursor = await channelCollection.find();
 
-    allCursor.forEach((thisChannel) => {
-        // TODO: Add channel deletion check here and removal from collection
-        client.channels.cache.get(thisChannel.channel_id).send(question);
-    })
+    // // Reset previous question for every channel to be the current top question
+    // channelCollection.update({}, { $set: {
+    //     prev_question : [questionTosave]
+    // }});
+
+    // allCursor.forEach((thisChannel) => {
+    //     // TODO: Add channel deletion check here and removal from collection
+    //     client.channels.cache.get(thisChannel.channel_id).send(question);
+    // })
 }
 
 async function GetAndSendQuestionToChannel(channelid, guildid, msg) {
@@ -168,13 +187,129 @@ async function SendQuestionToChannel(mongoclient, channelid, guildid, msg) {
     };
 }
 
-// <------------------------------------ MongoDB functions -------------------------------------------------->
+// <--------------------------------------- CS QOTD Sending Functions --------------------------------------->
 
-async function AddChannelToDatabase(mongoclient, channelid, guildid, msg) {
+async function GetAndSendCSQuestion() {
     try {
         // Connect to MongoDB Cluster
         //await mongoclient.connect();
-        await AddChannelIfExists(mongoclient, channelid, guildid, msg);
+        await SendDailyCS(mongoclient);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        //await mongoclient.close();
+    }
+}
+
+async function ReadGmail(access_token) {
+    const GMAIL_API_KEY = process.env.GMAIL_API_KEY;
+    const USER_ID = process.env.GMAIL_USER_ID;
+    const LABEL_ID = "Label_4014517504537073939"; // This label ID corresponds to the DCP label in the gmail
+    const GMAIL_LIST_URL = "https://gmail.googleapis.com/gmail/v1/users/" + USER_ID + "/messages?labelIds=" + LABEL_ID + "&key=" + GMAIL_API_KEY;
+    let gmail_list_request = {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer " + access_token
+        }
+    }
+
+    fetch(GMAIL_LIST_URL, gmail_list_request).then(response => {
+        return(response.json());
+    }).then(messageList => {
+        // First, get the most recent email id
+        console.log("Newest message ID: " + messageList.messages[0].id)
+        const MESSAGES_GET_URL = "https://gmail.googleapis.com/gmail/v1/users/" + USER_ID + "/messages/" + messageList.messages[0].id + "?&key=" + GMAIL_API_KEY;
+        let specific_message_request = {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + access_token
+            }
+        }
+        // Now, fetch the contents of the most recent email and decode it out of base64
+        fetch(MESSAGES_GET_URL, specific_message_request).then(response => {
+            return(response.json());
+        }).then(message => {
+            let email_contents = message.payload.parts[0].body.data;
+            let bufferObj  = Buffer.from(email_contents, "base64")
+            let decoded = bufferObj.toString("utf8");
+            //console.log(decoded);
+        })
+        // TODO: using the ID, use user.messages.get to get the contents and decode it
+        // Then, use mongodb to get the channel ids to send the contents to.
+    })
+}
+
+async function SendDailyCS(mongoclient) {
+    const GMAIL_REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+    const GMAIL_CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+    const GMAIL_CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET
+    const REFRESH_URL = "https://www.googleapis.com/oauth2/v4/token";
+    const post_body = `grant_type=refresh_token&client_id=${encodeURIComponent(GMAIL_CLIENT_ID)}&client_secret=${encodeURIComponent(GMAIL_CLIENT_SECRET)}&refresh_token=${encodeURIComponent(GMAIL_REFRESH_TOKEN)}`;
+    let refresh_request = {
+        body: post_body,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+
+    // post to the refresh endpoint, parse the json response and use the access token to call
+    fetch(REFRESH_URL, refresh_request).then(response => {
+        return(response.json());
+    }).then(response_json => {
+        console.log("Access token obtained.");
+        let access_token = response_json.access_token
+
+        const GMAIL_API_KEY = process.env.GMAIL_API_KEY;
+        const USER_ID = process.env.GMAIL_USER_ID;
+        const LABEL_ID = "Label_4014517504537073939"; // This label ID corresponds to the DCP label in the gmail
+        const GMAIL_LIST_URL = "https://gmail.googleapis.com/gmail/v1/users/" + USER_ID + "/messages?labelIds=" + LABEL_ID + "&key=" + GMAIL_API_KEY;
+        let gmail_list_request = {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + access_token
+            }
+        }
+
+        fetch(GMAIL_LIST_URL, gmail_list_request).then(response => {
+            return(response.json());
+        }).then(messageList => {
+            // First, get the most recent email id
+            console.log("Newest message ID: " + messageList.messages[0].id)
+            const MESSAGES_GET_URL = "https://gmail.googleapis.com/gmail/v1/users/" + USER_ID + "/messages/" + messageList.messages[0].id + "?&key=" + GMAIL_API_KEY;
+            let specific_message_request = {
+                method: "GET",
+                headers: {
+                    Authorization: "Bearer " + access_token
+                }
+            }
+            // Now, fetch the contents of the most recent email and decode it out of base64
+            fetch(MESSAGES_GET_URL, specific_message_request).then(response => {
+                return(response.json());
+            }).then(message => {
+                let email_contents = message.payload.parts[0].body.data;
+                let bufferObj  = Buffer.from(email_contents, "base64")
+                let decodedQuestion = bufferObj.toString("utf8");
+                let parsed_string = decodedQuestion.split("--------------------------------------------------------------------------------");
+                //console.log(parsed_string[0])
+                IterateAndSendToAll(mongoclient, "CSActiveChannels", parsed_string[0], parsed_string[0]);
+                //console.log(decoded);
+            })
+            // TODO: using the ID, use user.messages.get to get the contents and decode it
+            // Then, use mongodb to get the channel ids to send the contents to.
+        })
+        
+    })
+
+}
+
+// <------------------------------------ MongoDB functions -------------------------------------------------->
+
+async function AddChannelToDatabase(mongoclient, channelid, guildid, msg, collectionName) {
+    try {
+        // Connect to MongoDB Cluster
+        //await mongoclient.connect();
+        await AddChannelIfExists(mongoclient, channelid, guildid, msg, collectionName);
     } catch (e) {
         console.error(e);
     } finally {
@@ -182,9 +317,9 @@ async function AddChannelToDatabase(mongoclient, channelid, guildid, msg) {
     };
 }
 
-async function AddChannelIfExists(mongoclient, channelid, guildid, msg) {
-
-    let channelCollection = await mongoclient.db().collection("ActiveChannels");
+async function AddChannelIfExists(mongoclient, channelid, guildid, msg, collectionName) {
+    // Valid collections names: ActiveChannels (standard QOTD) and CSActiveChannels (CS QOTD)
+    let channelCollection = await mongoclient.db().collection(collectionName);
 
     // Check if the channel exists in the database
     let someCursor = await channelCollection.findOne(
@@ -195,7 +330,14 @@ async function AddChannelIfExists(mongoclient, channelid, guildid, msg) {
     )
     if (!someCursor) {
         console.log("Adding new channel with id: " + channelid);
-        msg.reply("QOTD has been added! Stay tuned for 9:00 AM PST, or try `!qotd_newq` for a question now!");
+        if (collectionName == "ActiveChannels") {
+            msg.reply("QOTD has been added! Stay tuned for 9:00 AM PST, or try `!qotd_newq` for a question now!");
+        } else if (collectionName == "CSActiveChannels") {
+            msg.reply("Daily Programming Question has been added! Stay tuned for 9:00 AM PST!")
+        } else {
+            console.log("No collection name of <" + collectionName + "> matched but new channel added");
+        }
+
         channelCollection.insertOne({
             channel_id : channelid,
             guild_id : guildid,
@@ -203,15 +345,21 @@ async function AddChannelIfExists(mongoclient, channelid, guildid, msg) {
         })
     } else {
         console.log(channelid + " already exists in database");
-        msg.reply("QOTD has already been started. Please wait until 9:00 AM PST, or try `!qotd_newq`!");
+        if (collectionName == "ActiveChannels") {
+            msg.reply("QOTD has already been started. Please wait until 9:00 AM PST, or try `!qotd_newq`!");
+        } else if (collectionName == "CSActiveChannels") {
+            msg.reply("Daily Programming Question has already been added! Please wait for 9:00 AM PST.");
+        } else {
+            console.log("No collection name of <" + collectionName + "> matches and current channel is already added");
+        }
     }
 }
 
-async function RemoveChannelFromDatabase(channelid, guildid, msg) {
+async function RemoveChannelFromDatabase(channelid, guildid, msg, collectionName) {
     try {
         // Connect to MongoDB Cluster
         //await mongoclient.connect();
-        await RemoveChannelIfExists(mongoclient, channelid, guildid, msg);
+        await RemoveChannelIfExists(mongoclient, channelid, guildid, msg, collectionName);
     } catch (e) {
         console.error(e);
     } finally {
@@ -219,9 +367,9 @@ async function RemoveChannelFromDatabase(channelid, guildid, msg) {
     }
 }
 
-async function RemoveChannelIfExists(mongoclient, channelid, guildid, msg) {
-
-    let channelCollection = await mongoclient.db().collection("ActiveChannels");
+async function RemoveChannelIfExists(mongoclient, channelid, guildid, msg, collectionName) {
+    // Valid collections names: ActiveChannels (standard QOTD) and CSActiveChannels (CS QOTD)
+    let channelCollection = await mongoclient.db().collection(collectionName);
 
     // Check if the channel exists in the database
     let someCursor = await channelCollection.findOne(
@@ -232,35 +380,64 @@ async function RemoveChannelIfExists(mongoclient, channelid, guildid, msg) {
     )
     if (someCursor) {
         console.log("Deleting channel " + channelid);
-        msg.reply("Stopping QOTD in this channel... D:");
+        if (collectionName == "ActiveChannels") {
+            msg.reply("Stopping QOTD in this channel... D:");
+        } else if (collectionName == "CSActiveChannels") {
+            msg.reply("Terminated Daily Programmign Question from channel.")
+        } else {
+            console.log("Attempting to delete channel from <" + collectionName + "> but cannot find match to collection")
+        }
+        
         channelCollection.deleteOne({
             channel_id : channelid,
             guild_id : guildid
         }, true)
     } else {
         console.log("Cannot delete channel that doesn't exist");
-        msg.reply("It appears you haven't added QOTD to this channel yet. At least try me out before removing me... D:");
+        if (collectionName == "ActiveChannels") {
+            msg.reply("It appears you haven't added QOTD to this channel yet. At least try me out before removing me... D:");
+        } else if (collectionName == "CSActiveChannels") {
+            msg.reply("Programming QOTD has not been added yet!");
+        } else {
+            console.log("Attempting to delete channel from <" + collectionName + "> when it doesn't exist in database, but cannot find match to collection")
+        }
+        
     }
 }
 
+
+// <-------------------------------------- Discord.js Control the sending of messages --------------------------------------------->
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-
-// <-------------------------------------- Discord.js Control the sending of messages --------------------------------------------->
 client.on("message", msg => {
     // Starting the Bot
     if (msg.content === "!qotd_start") {
         // Add the channel to the mongodb database
         let [channelid, guildid] = GetMessageIDs(msg);
-        AddChannelToDatabase(mongoclient, channelid, guildid, msg);
+        AddChannelToDatabase(mongoclient, channelid, guildid, msg, "ActiveChannels");
+    }
+
+    if (msg.content === "!npm start") {
+        // Add the channel to the mongodb database
+        let [channelid, guildid] = GetMessageIDs(msg);
+        AddChannelToDatabase(mongoclient, channelid, guildid, msg, "CSActiveChannels");
     }
     
     // Remove scheduled QOTD from current channel
     if (msg.content ==="!qotd_stop") {
         let [channelid, guildid] = GetMessageIDs(msg);
-        RemoveChannelFromDatabase(channelid, guildid, msg);
+        RemoveChannelFromDatabase(channelid, guildid, msg, "ActiveChannels");
+    }
+
+    if (msg.content ==="!ctrl c") {
+        let [channelid, guildid] = GetMessageIDs(msg);
+        RemoveChannelFromDatabase(channelid, guildid, msg, "CSActiveChannels");
+    }
+
+    if (msg.content ==="!cstest") {
+        GetAndSendCSQuestion();
     }
 
     // Instantly Generating another question
@@ -300,3 +477,4 @@ client.login(BOT_TOKEN);
 // TODO: Toggle filter option
 // TODO: Reset once week instead?
 // TODO: Don't save the 404 question not found
+// TODO: Error checking on channel collection parameter?
